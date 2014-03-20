ad8baa0a05e2fcd5541ce99924604ae8eb5f4d2e
Peter Weir <weir@caltech.edu>
2014-03-20 12:31:42 -0700

    Added docstring to confidence_interval

--- bootstrap.py
+++ bootstrap.py
@@ -36,7 +36,33 @@ def _ma_confidence_interval_1d(A, confidenceLevel=.05, metric=np.mean, numResamp
     return confidenceInterval
 
 def confidence_interval(A, axis=None, confidenceLevel=.05, metric=np.mean, numResamples=10000, interpolate=True):
-    """Calculates bootstrap confidence interval along the given axis"""
+    """Return the bootstrap confidence interval of an array or along an axis ignoring NaNs and masked elements.
+    
+    Parameters
+    ----------
+    A : array_like
+        Array containing numbers whose confidence interval is desired. 
+    axis : int, optional
+        Axis along which the confidence interval is computed.
+        The default is to compute the confidence interval of the flattened array.
+    confidenceLevel: float or array, optional
+        confidence level of confidence interval. If length-n array, n confidence intervals will be computed
+        The default is .05
+    metric : numpy function, optional
+        metric to calculate confidence interval for.
+        The default is numpy.mean
+    numResamples : int, optional
+        number of bootstrap samples. The default is 10000.
+    interpolate: bool, optional
+        uses scipy.stats.scoreatpercentile to interpolate between bootstrap samples if confidenceLevel*numResamples/2.0 is not integer.
+        The default is True
+        
+    Returns
+    -------
+    confidenceInterval : ndarray
+    An array with the same shape as `A`, with the specified axis replaced by one twice the length of the confidenceLevel
+    If `A` is a 0-d array, or if axis is None, a length-2 ndarray is returned.
+    """
     
     if interpolate is True and scoreatpercentile is False:
         print "need scipy to interpolate between values"
