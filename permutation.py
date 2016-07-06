753a5d01b0cd0920e8293b10b0586881a517cc57
Peter Weir <weir@caltech.edu>
2016-07-06 14:47:14 -0700

    added exact_p_val method

--- permutation.py
+++ permutation.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import itertools
 
 def _p_val_1d(A, B, metric=np.mean, numResamples=10000):
     """Return p value of observed difference between 1-dimensional A and B"""
@@ -116,5 +118,54 @@ def fast_p_val(A, B, axis=0, metric=np.mean, numResamples=10000):
         resampleDiffs[resampleInd,...] = diff
         
     pVal = (np.sum(resampleDiffs > observedDiff,axis=0) + np.sum(resampleDiffs < -observedDiff,axis=0))/float(numResamples)
+        
+    return pVal
+
+def exact_p_val(A, B, axis=0, metric=np.mean):
+    """Return the p value that metric(A) and metric(B) differ along an axis. By computing all
+    permutations of the union of A and B with corresponding sizes
+    
+    Parameters
+    ----------
+    A : array_like
+        Array containing numbers of first group. 
+    B : array_like
+        Array containing numbers of second group. 
+    axis : int, optional
+        Axis along which the p value is computed.
+        The default is to compute the p value along the first dimension.
+    metric : numpy function, optional
+        metric to calculate p value for.
+        The default is numpy.mean
+        
+    Returns
+    -------
+    pValue : ndarray
+        An array with the same shape as `A` and `B`, with the specified axis removed.
+    
+    See Also
+    --------
+    p_val : for larger sample sizes, in which computing all permutations is infeasible
+    """
+    rolledA = np.rollaxis(A,axis)
+    rolledB = np.rollaxis(B,axis)
+    
+    observedDiff = abs(metric(rolledA,axis=0) - metric(rolledB,axis=0))
+    combined = np.concatenate((rolledA, rolledB),axis=0)
+    numA = np.shape(rolledA)[0]
+    numB = np.shape(rolledB)[0]
+    
+    resampleShape = np.array(combined.shape)
+    resampleShape[0] = math.factorial(numA + numB)/(math.factorial(numA)*math.factorial(numB))
+    resampleDiffs = np.zeros(resampleShape,dtype='float')
+
+    for resampleInd, indsToInclude in enumerate(itertools.combinations(range(numA + numB), numA)):
+        indsToExclude = [i for i in range(12) if i not in indsToInclude]
+        permutedA = combined[indsToInclude, ...]
+        permutedB = combined[indsToExclude, ...]
+        diff = metric(permutedA, axis=0) - metric(permutedB, axis=0)
+        resampleDiffs[resampleInd,...] = diff
+        
+    pVal = (np.sum(resampleDiffs > observedDiff, axis=0) + np.sum(resampleDiffs < -observedDiff, axis=0))/float(resampleShape[0])
         
     return pVal
